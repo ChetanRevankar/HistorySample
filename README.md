22b0e50a84d4f8387f2493b3803888d040aceca9
ChetanRevankar <chetananantrevankar@gmail.com>
2021-04-17 17:51:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-grycij
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-grycij)
---
+++ src/app/mock-products.ts
@@ -0,0 +1,28 @@
+import { Product } from "./product";
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: "Phone XL",
+    price: 799,
+    description: "A large phone with one of the best screens"
+  },
+  {
+    id: 2,
+    name: "Phone Mini",
+    price: 699,
+    description: "A great phone with one of the best cameras"
+  },
+  {
+    id: 3,
+    name: "Phone Standard",
+    price: 299,
+    description: "Budget Phone good for normal use"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
